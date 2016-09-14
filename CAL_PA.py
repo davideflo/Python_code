7857a6085fa8f0431e4ef757dff7e35930058ba6
davideflo <davideflo@users.noreply.github.com>
2016-09-14 17:27:01 +0200

    analysis CAL products (server)

---
+++ CAL_PA.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 12:12:21 2016
+
+@author: utente
+
+Analysis for CAL products
+"""
+
+import pandas as pd
+
+cal = pd.read_excel('Documents/CAL.xlsx', sheetname = 'valori CAL')
