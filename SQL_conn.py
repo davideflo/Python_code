de87fd8c1647bc8b4852afb1904570e4bb36a285
davideflo <davideflo@users.noreply.github.com>
2016-11-03 17:24:54 +0100

    connection now working

--- SQL_conn.py
+++ SQL_conn.py
@@ -27,9 +27,14 @@ cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.0.60;PORT=1521;DATABAS
 cnxn = pyodbc.connect(driver='{SQL Server}', host='192.168.0.60', port='1521', database='T_SYS_CODIFICA_PARAM',
                       trusted_connection='yes', user='read_only', password='read_only')
 
-cnxn = pyodbc.connect(driver='{ODBC-SQL Server}', host='192.168.0.60', database='T_SYS_CODIFICA_PARAM',
+cnxn = pyodbc.connect(driver='{SQL Server}', host='192.168.0.60', database='argon',
                       trusted_connection='yes', uid='read_only', password='read_only')
 
+cnxn = pyodbc.connect(driver='{Oracle in Oradb11g_home1}', host='192.168.0.60', database='ARGON',
+                      trusted_connection='yes', uid='read_only', password='read_only')
+
+connectString = 'Driver={Oracle in Oradb11g_home1};Server=pippo:1521/argon.pippo;uid= read_only;pwd=read_only'
+cnxn = pyodbc.connect(connectString)
 dir(odbc)
 
 datasource = odbc.Windows.DataSources
@@ -47,4 +52,51 @@ def show_odbc_sources():
 ####################################################################################################
 
 show_odbc_sources()
+
+connectString = 'Driver={Microdsoft ODBC for Oracle};Server=192.168.0.60:1521/argon.192.168.0.60;uid= read_only;pwd=read_only'
+cnxn = pyodbc.connect(connectString)
+
+####################################################################################################
+import os
+import cx_Oracle
+import csv
+ 
+SQL="SELECT * FROM SOME_TABLE"
+ 
+# Network drive somewhere
+filename="S:\Output.csv"
+FILE=open(filename,"w");
+output=csv.writer(FILE, dialect='excel')
+ 
+# You can set these in system variables but just in case you didnt
+os.putenv('ORACLE_HOME', '/oracle/product/10.2.0/db_1') 
+os.putenv('LD_LIBRARY_PATH', '/oracle/product/10.2.0/db_1/lib') 
+ 
+dns = cx_Oracle.makedsn('192.168.0.60', '1521', 'argon')
+connection = cx_Oracle.connect('read_only', 'read_only', dns) 
+cursor = connection.cursor()
+
+SQL = "SELECT * FROM MNH_BILLING.AUDIT_MISURE_GAS_CAMBI_STATO"
+
+cursor.execute(SQL)
+
+for row in cursor:
+    print row
+    
+cursor.close()
+connection.close()
+FILE.close()
+
+
+
+db = cx_Oracle.connect('hr', 'hrpwd', 'localhost:1521/XE')
+db1 = cx_Oracle.connect('hr/hrpwd@localhost:1521/XE')
+dsn_tns = cx_Oracle.makedsn('localhost', 1521, 'XE')
+print dsn_tns
+'(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SID=XE)))'
+db2 = cx_Oracle.connect('hr', 'hrpwd', dsn_tns)
+ 
+
+
+
 
