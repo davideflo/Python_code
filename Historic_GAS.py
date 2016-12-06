6c610b880a0a787aefb1bfea1329fd2a9040632b
davideflo <davideflo@users.noreply.github.com>
2016-12-06 17:26:17 +0100

    complete GAS historical data

--- Historic_GAS.py
+++ Historic_GAS.py
@@ -9,12 +9,42 @@ Construction of GAS historical Consumption
 from __future__ import division
 import pandas as pd
 import cx_Oracle 
+import numpy as np
+from collections import OrderedDict
 import datetime
+
+####################################################################################################
+def FilterMisura(df, pdr, year, month):
+    df2 = df.ix[df['pdr'] == pdr]
+    df3 = df2.ix[df2['year'] == year]
+    df4 = df3.ix[df3['month'] == month]
+    if df4.shape[0] == 1:
+        return df4['lettura']
+    else:
+        return 'unknown'
+####################################################################################################
 
 
 dns = cx_Oracle.makedsn('192.168.0.60', '1521', 'argon')
 connection = cx_Oracle.connect('read_only', 'read_only', dns) 
 cursor = connection.cursor()
+
+SQLmisura = """SELECT D_LETTURA,
+               S_PDR,
+               CD_TP_LETTURA
+               FROM MNH_LOGISTICA.T_LETTURE_GAS
+               """
+
+cursor.execute(SQLmisura)
+data2 = cursor.fetchall()
+
+ii = 0
+
+dfm = pd.DataFrame()
+
+for j in range(len(data2)):
+    dfm = dfm.append(pd.DataFrame({'pdr':str(data2[j][1]), 'year':data2[j][0].year, 'month':data2[j][0].month, 'lettura':data2[j][2]}, index = [ii]))
+    ii += 1
 
 SQL181 = """SELECT ANNO_COMPETENZA, 
             CLIENTE,
@@ -40,13 +70,16 @@ data = cursor.fetchall()
 
 pdr = []
 for i in range(len(data)):
-    pdr.append(data[i][3])
+    pdr.append(str(data[i][3]))
     
 pdr = list(set(pdr))
 
 df = pd.DataFrame()
 
 si = 0
+p = pdr[0]
+df2 = pd.DataFrame()
+
 for p in pdr:
     if p is not None:
         loc = []
@@ -55,23 +88,49 @@ for p in pdr:
             if p in d:
                 loc.append(d)
                 anno.append(d[0])
+        cliente = loc[0][1]
+        cd_cli = loc[0][2]
+        ppdr = loc[0][3]
+        mis = loc[0][4]
+        corre = loc[0][5]
+        distr = loc[0][6]
         for a in list(set(anno)):
+            dia = OrderedDict()
             loc_anno = []
             for l in loc:
-                if l[0] == a:
-                    loc_anno.append(l)
-            for la in loc_anno:
-                loc_anno_mese = []
-                for m in range(1,13,1):                
-                    if la[9] == m:
-                        loc_anno_mese.append(la)
-                    consumo = 0
-                    for lam in loc_anno_mese:
-                        if lam[10] is not None:
-                            consumo += lam[10]
-                    ldf = pd.DataFrame({'cliente': la[1],'cod_cliente': la[2],'pdr': la[3],'anno_competenza': a,
-                                        'matricola_misuratore': la[4],'matricola_correttore': la[5],'distributore': la[6],
-                                        'mese_competenza': m, 'consumo_smc': consumo}, index = [si])
-                    si += 1
-                    df = df.append(ldf)
-                
+                if l[0] == a:                    
+                    for m in range(1,13,1):                
+                        if l[9] == m:
+                            if m in dia.keys():
+                                dia[m].append(l[10])
+                            else:
+                                dia[m] = [l[10]]
+            for m in range(1,13,1):
+                misura = FilterMisura(dfm, ppdr, a, m)
+                if m in dia.keys():
+                    cons = [x for x in dia[m] if x is not None]
+                    ldf = pd.DataFrame({'cliente': cliente,'cod_cliente': cd_cli,'pdr': ppdr,'anno_competenza': a,
+                                                'matricola_misuratore': mis,'matricola_correttore': corre,'distributore': distr,
+                                                'mese_competenza': m, 'consumo_smc': np.sum(cons), 'tipo_misura': misura}, index = [si])
+                else:
+                    ldf = pd.DataFrame({'cliente': cliente,'cod_cliente': cd_cli,'pdr': ppdr,'anno_competenza': a,
+                                                'matricola_misuratore': mis,'matricola_correttore': corre,'distributore': distr,
+                                                'mese_competenza': m, 'consumo_smc': 0, 'tipo_misura': misura}, index = [si])
+                si += 1
+                df = df.append(ldf)
+
+#####################################################################################################                
+#def changeencode(data, cols):
+#    import string
+#    printable = set(string.printable)   
+#    for col in cols:
+#        for i in range(data.shape[0]):            
+#            data[col].ix[i] = filter(lambda x: x in printable, data[col].ix[i])
+#    return data   
+#####################################################################################################
+
+#df2 = changeencode(df, ['cliente', 'distributore'])
+
+df.to_csv('storico_GAS.csv', sep = '|')
+#df2.to_excel('storico_GAS.xlsx', encoding='utf-8')
+
