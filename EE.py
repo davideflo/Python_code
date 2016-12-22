4f55d9fdb8b97fcb6bf4f9fbe4682df03ab80a89
davideflo <davideflo@users.noreply.github.com>
2016-12-22 17:01:04 +0100

    reminder

--- EE.py
+++ EE.py
@@ -9,6 +9,7 @@ script to use EE_interp.py from command line
 
 import sys
 import EE_interp
+import datetime
 
 #path = 'prediction_PUN_2016-09-02.xlsx'
 ### when put path as an argument in command line, remove "'" (hyphen)
@@ -18,4 +19,21 @@ df = EE_interp.Extract_Dataset(path)
 
 print 'dimensions of the updated dataset: {}'.format(df.shape)
 
+today = datetime.date.today()
+    
+if today.day == 1:
+    print '#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@'
+    print '#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@'
+    print '#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@'
+    print '#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@'
+    print ' '
+    print ' '
+    print 'UPDATE THE METEO FILES OF THE CITIES'
+    print ' '
+    print ' '
+    print '#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@'
+    print '#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@'
+    print '#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@'
+    print '#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@'
+    
 #EE_interp.get_InterpolatedCurves(path)
