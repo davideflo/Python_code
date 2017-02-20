551ca89199b6ffea7d447e7c3f850b8fa5ff8188
davideflo <davideflo@users.noreply.github.com>
2017-02-20 17:47:04 +0100

    New model trial

--- Sbilanciamento.py
+++ Sbilanciamento.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import statsmodels.api
 import calendar
 import scipy
-from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from collections import OrderedDict
 import datetime
@@ -80,8 +80,23 @@ def AddHolidaysDate(vd):
   
     return holidays
 ####################################################################################################
+def GetMeanCurve(df, var):
+    mc = OrderedDict()
+    for y in [2015, 2016]:
+        dfy = df[var].ix[df.index.year == y]
+        for m in range(1,13,1):
+            dfym = dfy.ix[dfy.index.month == m]
+            Mean = []
+            for h in range(24):
+                dfymh = dfym.ix[dfym.index.hour == h].mean()
+                Mean.append(dfymh)
+            mc[str(m) + '_' + str(y)] = Mean
+    mc = pd.DataFrame.from_dict(mc, orient = 'index')
+    return mc
+####################################################################################################
 def MakeDatasetTS(df, meteo):
     dts = OrderedDict()
+    #mc = GetMeanCurve(df, 'FABBISOGNO REALE')
     for i in df.index.tolist():
         wd = i.weekday()
         h = i.hour
@@ -94,6 +109,29 @@ def MakeDatasetTS(df, meteo):
     dts = pd.DataFrame.from_dict(dts, orient = 'index')
     return dts
 ####################################################################################################
+def MakeDatasetTSCurve(df, meteo):
+    dts = OrderedDict()
+    for i in df.index.tolist():
+        m = i.month
+        y = i.year
+        dfm = df.ix[df.index.month == m]
+        dfmy = dfm.ix[dfm.index.year == y]
+        wd = i.weekday()
+        h = i.hour
+        dy = i.timetuple().tm_yday
+        Tmax = meteo['Tmax'].ix[meteo['DATA'] == i.date()].values.ravel()[0]
+        rain = meteo['PIOGGIA'].ix[meteo['DATA'] == i.date()].values.ravel()[0]
+        wind = meteo['VENTOMEDIA'].ix[meteo['DATA'] == i.date()].values.ravel()[0]
+        hol = AddHolidaysDate(i.date())
+        cm = GetMeanCurve(dfmy.ix[dfmy.index.date <= pd.to_datetime(i).date()],'FABBISOGNO REALE').dropna().values.ravel()
+        ll = [wd, h, dy, Tmax, rain, wind, hol]
+        ll.extend(cm.tolist())
+        ll.extend([df['FABBISOGNO REALE'].ix[i]])
+        dts[i] =  ll
+    dts = pd.DataFrame.from_dict(dts, orient = 'index')
+    return dts
+####################################################################################################
+
 
 sbil = pd.read_excel('C:/Users/utente/Documents/misure/aggregato_sbilanciamento.xlsx')
 nord = sbil.ix[sbil['CODICE RUC'] == 'UC_DP1608_NORD']
@@ -166,14 +204,17 @@ plt.plot(diff)
 
 ##### experiment AdaBoost + Decision Trees 2015 to 2016
 cnord = sbil.ix[sbil['CODICE RUC'] == 'UC_DP1608_CNOR']
-cnord.index = pd.date_range('2015-01-01', '2017-01-02', freq = 'H')[:nord.shape[0]]
+cnord.index = pd.date_range('2015-01-01', '2017-01-02', freq = 'H')[:cnord.shape[0]]
 fi5 = pd.read_excel('C:/Users/utente/Documents/PUN/Firenze 2015.xlsx')
+fi5 = fi5.ix[:364].set_index(pd.date_range('2015-01-01', '2015-12-31', freq = 'D'))
 fi6 = pd.read_excel('C:/Users/utente/Documents/PUN/Firenze 2016.xlsx')
+fi6 = fi6.ix[:365].set_index(pd.date_range('2016-01-01', '2016-12-31', freq = 'D'))
+
 
 DT5 = MakeDatasetTS(cnord.ix[cnord.index.year == 2015], fi5)
 DT6 = MakeDatasetTS(cnord.ix[cnord.index.year == 2016], fi6)
 
-regr = AdaBoostRegressor(DecisionTreeRegressor(max_depth = 24),n_estimators=24000)
+regr = AdaBoostRegressor(DecisionTreeRegressor(max_depth = 24),n_estimators=5000)
 
 DT5s = DT5.sample(frac = 1).reset_index(drop = True)
 
@@ -198,10 +239,58 @@ plt.plot(y - yhat)
 
 yhat6 = regr.predict(x6)
 
+regr6R2 = 1 - (np.sum((y6 - yhat6)**2))/(np.sum((y6 - np.mean(y6))**2))
+
+
 plt.figure()
 plt.plot(yhat6, color = 'navy', marker = 'o')
 plt.plot(y6.values.ravel(), color = 'coral')
 
 plt.figure()
 plt.plot(y6 - yhat6)
+(y6 - yhat6).ix[(y6 - yhat6).index.month >= 8].mean()
+(y6 - yhat6).ix[(y6 - yhat6).index.month < 8].mean()
+(y6 - yhat6).ix[(y6 - yhat6).index.month >= 8].mean()
+(y6 - yhat6).ix[(y6 - yhat6).index.month < 8].mean()
 
+
+rfregr = AdaBoostRegressor(RandomForestRegressor(criterion = 'mse', max_depth = 24), n_estimators=3000)
+rfregr.fit(xs, ys)
+yhat = rfregr.predict(x)
+
+regrR2 = 1 - (np.sum((y - yhat)**2))/(np.sum((y - np.mean(y))**2))
+
+yhat6 = rfregr.predict(x6)
+regr6R2 = 1 - (np.sum((y6 - yhat6)**2))/(np.sum((y6 - np.mean(y6))**2))
+
+plt.figure()
+plt.plot(yhat6, color = 'navy', marker = 'o')
+plt.plot(y6.values.ravel(), color = 'coral')
+
+plt.figure()
+plt.plot(y6 - yhat6)
+(y6 - yhat6).ix[(y6 - yhat6).index.month >= 8].mean()
+(y6 - yhat6).ix[(y6 - yhat6).index.month < 8].mean()
+(y6 - yhat6).ix[(y6 - yhat6).index.month >= 8].std()
+(y6 - yhat6).ix[(y6 - yhat6).index.month < 8].std()
+
+mc = GetMeanCurve(cnord, 'FABBISOGNO REALE')
+
+mc15 = mc.ix[mc.index[:12]]
+mc16 = mc.ix[mc.index[12:]]
+
+mc15.T.plot(legend = False)
+mc16.T.plot(legend = False)
+ydiff = mc16.reset_index(drop = True) - mc15.reset_index(drop = True)
+ytdiff = fi6['Tmedia'].resample('M').mean().values.ravel() - fi5['Tmedia'].resample('M').mean().values.ravel()
+
+plt.figure()
+plt.plot(ytdiff)
+
+plt.figure()
+ydiff.T.plot(legend = False)
+plt.axhline(y = 0)
+
+fi = fi5.append(fi6)
+
+DTC = MakeDatasetTSCurve(cnord, fi)
