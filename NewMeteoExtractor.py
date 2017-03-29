b6581b5774e1d3d2ffa9654e0956400610cee443
davideflo <davideflo@users.noreply.github.com>
2017-03-29 17:19:11 +0200

    Extractor finally correctly working

--- NewMeteoExtractor.py
+++ NewMeteoExtractor.py
@@ -16,10 +16,10 @@ import pandas as pd
 import numpy as np
 import datetime
 from collections import OrderedDict
-from PyQt4.QtGui import *  
-from PyQt4.QtCore import *  
-from PyQt4.QtWebKit import *  
-from lxml import html 
+#from PyQt4.QtGui import *  
+#from PyQt4.QtCore import *  
+#from PyQt4.QtWebKit import *  
+#from lxml import html 
 import re
 import time
 
@@ -30,19 +30,19 @@ import time
 #### Or the second answer here:
 #### http://stackoverflow.com/questions/373335/how-do-i-get-a-cron-like-scheduler-in-python
 
-####################################################################################################
-class Render(QWebPage):  
-  def __init__(self, url):  
-    self.app = QApplication(sys.argv)  
-    QWebPage.__init__(self)  
-    self.loadFinished.connect(self._loadFinished)  
-    self.mainFrame().load(QUrl(url))  
-    self.app.exec_()  
-    self.frame = self.mainFrame()
-  
-  def _loadFinished(self, result):  
-    self.frame = self.mainFrame()  
-    self.app.quit() 
+#####################################################################################################
+#class Render(QWebPage):  
+#  def __init__(self, url):  
+#    self.app = QApplication(sys.argv)  
+#    QWebPage.__init__(self)  
+#    self.loadFinished.connect(self._loadFinished)  
+#    self.mainFrame().load(QUrl(url))  
+#    self.app.exec_()  
+#    self.frame = self.mainFrame()
+#  
+#  def _loadFinished(self, result):  
+#    self.frame = self.mainFrame()  
+#    self.app.quit() 
 ####################################################################################################
 
 
@@ -50,32 +50,35 @@ def Extractor():
     cities = ['Milano-domani-15146', 'Firenze-domani-48017', 'Roma-domani-58091', 
     'Bari-domani-72006', 'Palermo-domani-82053', 'Cagliari-domani-92009']
     
-#    city = cities[2]
+#    city = cities[0]
     for city in cities:
         
         
         path = 'C:/Users/utente/Documents/meteo'
-        #cromepath = r'C:/Users/utente/Desktop/chromedriver/chromedriver.exe'
+        cromepath = r'C:/Users/utente/Desktop/chromedriver/chromedriver.exe'
         
-        #browser = webdriver.Chrome(cromepath)
+        browser = webdriver.Chrome(cromepath)
         
         url = 'http://www.meteo.it/meteo/' + city
         
-        #browser.get(target_url)
-
-        start_time = time.time()
+        browser.get(url)
+        
+        page = browser.page_source
+        result = page
+#        start_time = time.time()
 ### forse si blocca per questo:
 # http://stackoverflow.com/questions/21909907/pyqt-class-not-working-for-the-second-usage/21918243#21918243        
-        r = Render(url)  
-        result = r.frame.toHtml()
-        print("--- %s seconds ---" % (time.time() - start_time))  
+#        r = Render(url)  
+#        result = r.frame.toHtml()
+#        del r
+#        print("--- %s seconds ---" % (time.time() - start_time))  
 
         ### find the temperatures  
         temp = [m.start() for m in re.finditer(";deg;", result)]
         Temp = [float(re.findall(r'\d+', result[temp[i]:temp[i]+12])[0]) for i in range(len(temp))]
         ### find the wind r'Part 1(.*?)Part 3'
         wind = [m.start() for m in re.finditer("pk_cventi", result)]
-        Vento2 = [re.findall(r'\d+', result[wind[i]:wind[i]+150])[-2] for i in range(len(wind))]
+        Vento2 = [re.findall(r'\d+', result[wind[i]:wind[i]+200])[-2] for i in range(len(wind))]
         Vento1 = ['.' + re.findall(r'\d+', result[wind[i]:wind[i]+150])[-1] for i in range(len(wind))]
         Vento = [float(Vento2[i] + Vento1[i]) for i in range(24)]
         ### find the rain 
@@ -83,6 +86,7 @@ def Extractor():
         Pioggia = [float(re.findall(r'\d+', result[rain[i]-5:rain[i]])[0]) for i in range(len(rain))]
         
         print 'Done extracting the values'
+        
 #        c_url = browser.current_url        
 #        r = requests.get(c_url)
 #        data = r.text        
@@ -129,7 +133,7 @@ def Extractor():
         dom['Tmedia'] = [np.mean(Temp)]
         dom['vento'] = [np.mean(Vento)]
         dom['pioggia'] = [P]
-        dom['mmpioggia'] = [np.mean(Pioggia)]
+        dom['mmpioggia'] = [np.sum(Pioggia)]
     
         domani = pd.DataFrame.from_dict(dom, orient = 'columns')
         
