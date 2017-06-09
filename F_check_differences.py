aaccb5e6f22b033a33491b03f7c792f56f9eb7cc
davideflo <davideflo@users.noreply.github.com>
2017-06-09 17:40:49 +0200

    SOSExtractor()

--- F_check_differences.py
+++ F_check_differences.py
@@ -12,7 +12,10 @@ import numpy as np
 from collections import OrderedDict
 import datetime
 import calendar
-
+import re
+import time
+import os
+from bs4 import BeautifulSoup as Soup
 
 ###############################################################################
 def ReduceDF(x):
@@ -49,14 +52,22 @@ def GetData(pdos):
             for e in ea:                
                 ATP3 = ATP2.ix[ATP2['Ea'] == e]                
                 if ATP3.shape[0] > 1:
-                    ATP4 = ATP3.ix[ATP3['CodFlusso'] != 'PDO']
-                    ATP4 = ATP4.ix[max(ATP4.index)]
+                    ATPpdo = ATP3.ix[ATP3['CodFlusso'] == 'PDO']
+                    ATPnpdo = ATP3.ix[ATP3['CodFlusso'] != 'PDO']
+                    if ATPpdo.shape[0] > 0 and ATPnpdo.shape[0] > 0:
+                        ATP4 = ATPnpdo.ix[max(ATPnpdo.index)]
+                    elif ATPpdo.shape[0] > 0 and ATPnpdo.shape[0] == 0:
+                        ATP4 = ATPpdo.ix[max(ATPpdo.index)]
+                    elif ATPpdo.shape[0] == 0 and ATPnpdo.shape[0] > 0:
+                        ATP4 = ATPnpdo.ix[max(ATPnpdo.index)]
+                    else:
+                        pass
                 else:
                     ATP4 = ATP3
                 if ATP4.shape[0] > 0:
                     ll = []
                     dt = datetime.date(int(ma[3:]), int(ma[:2]), int(e))
-                    zona = ATP4['PuntoDispacciamento']#.values[0]
+                    zona = ATP4['PuntoDispacciamento'] if isinstance(ATP4['PuntoDispacciamento'], str) else ATP4['PuntoDispacciamento'].values[0]
                     ll.append(i)
                     ll.append(dt)  
                     ll.append(zona)
@@ -109,7 +120,8 @@ pdos3 = ToDF(pdos2)
 Pdo = GetData(pdos3)
 
 Pdo = Pdo.ix[Pdo['DATA'] > datetime.date(2016,12,31)]
-Pdo = Pdo.ix[Pdo['CodFlusso'] == 'PDO']
+
+
 
 nord = ToTS(Pdo, "NORD")
 nord = nord.apply(lambda x: x/1000)
@@ -151,230 +163,73 @@ diff.mean()
 diff.std()
 diff.median()
 
-pd.tools.plotting.autocorrelation_plot(diff)
 
 
-import xml.etree.ElementTree as ET
-import os
-import glob
-path2 = 'Z:/AREA BO&B/23.P-RNO - PD-RFO DISTRIBUTORI - BONUS/2017/01. Invio di Gennaio/SOS/'
-
-for infile in glob.glob( os.path.join(path, '*.xml') ):
-        tree = ET.parse(infile)
-        root = tree.getroot()
-        with open(infile+'new.csv','w') as outfile:
-            for elem in root.findall('.//event[@type="MEDIA"]'):
-                    mediaidelem = elem.find('./mediaid')
-                    if mediaidelem is not None:
-                            outfile.write("{}\n".format(mediaidelem.text))
-
-for infile in glob.glob( os.path.join(path, '*.xml') ):
-    try:
-        tree = ET.parse(infile)
-        root = tree.getroot()
-        print(root)
-    except ET.ParseError as e:
-        print(infile, str(e))
-        continue
 ###############################################################################
-import os
-import xml.parsers.expat
-from xml.sax.saxutils import escape
-from optparse import OptionParser
-from math import log10
+##################### SOS EXTRACTION ##########################################
+###############################################################################
 
 
-# How much data we process at a time
-CHUNK_SIZE = 100 * 1024 * 1024
-
-# The sequence of element leading us to the current one
-path = []
-
-# How far we are in the current file
-cur_size = 0
-# From how much should we start another file
-MAX_SIZE = 100 * 1024*1024 # 1Mb
-
-# The current index
-cur_idx = 0
-# The current file handle we are writing to
-cur_file = None
-
-# The format string used to introduce the index in the file to be written
-FMT = ".%d"
-
-# The filename we are playing with
-out_dir = None
-root = None
-ext = None
-
-# The xml declaration of the file.
-xml_declaration = None
-
-# What was the signature of the last start element
-start = None
-
-# if we are currently in the process of changing file
-ending = False
-
-def attrs_s(attrs):
-    """ This generate the XML attributes from an element attribute list """
-    l = ['']
-    for i in range(0,len(attrs), 2):
-        l.append('%s="%s"' % (attrs[i], escape(attrs[i+1])))
-    return ' '.join(l)
-
-def next_file():
-    """ This makes the decision to cut the current file and starta new one """
-    global cur_size, ending
-    if (not ending) and (cur_size > MAX_SIZE):
-        # size above threshold, and not already ending
-        global cur_file, cur_idx
-        print("part %d Done" % cur_idx)
-        ending = True
-        # Close the current elements
-        for elem in reversed(path):
-            end_element(elem[0])
-        # Close the file
-        cur_file.close()
-        # reset the size
-        cur_size = 0
-        # Open another file
-        cur_idx += 1
-        cur_file = open(os.path.join(out_dir, root + FMT % cur_idx + ext),
-                        'wt')
-        if xml_declaration is not None:
-            cur_file.write('<?xml%s?>\n' % attrs_s(xml_declaration))
-        # Start again where we stopped
-        for elem in path:
-            start_element(*elem)
-        # We are done 'ending'
-        ending = False
 
 
-def xml_decl(version, encoding, standalone):
-    global xml_declaration
-    l = ['version', version, 'encoding', encoding]
-    if standalone != -1:
-        l.extend(['standalone', 'yes' if standalone else 'no'])
-    xml_declaration = l
-    cur_file.write('<?xml%s?>\n' % attrs_s(xml_declaration))
+###############################################################################
+def Replacer(s):
+    return s.replace(",",".")
+###############################################################################    
+def ReMeasureExtractor(s):
+    E = [re.findall(r'"(.*?)"', s)][0]
+    E = E[1:]
+    mis = list(map(Replacer, E))
+    mis2 = list(map(float, mis))
+    return mis2
+###############################################################################    
+def SOSExtractor(infile):    
+    count = 0
+    dix = OrderedDict()
+    pdox = Soup(open(infile).read(), "xml")
+    bs = pdox.find_all('DatiPod')
+    start_time = time.time()
+    for b in bs:
+        pod = b.find_all('Pod')
+        M = b.find_all('MeseAnno')[:2]
+        M = str(M)[11:18]
+        y = int(M[3:])
+        m = int(M[:2])
+        Er = b.find_all('Ea')
+        for er in Er:
+            tbi = []
+            day = str(er)[(str(er).find(">")+1):(str(er).find(">")+3)] 
+            print(day)
+            mis = ReMeasureExtractor(str(er))
+            if len(mis) < 96:
+                pass
+            else:
+                tbi.append(str(pod[0])[5:19])
+                tbi.append(day)
+                tbi.append(datetime.date(y,m,int(day)))
+                tbi.extend(mis)
+                dix[count] = tbi
+                count += 1
+    print("--- %s seconds ---" % (time.time() - start_time))
+    dix = pd.DataFrame.from_dict(dix, orient = 'index')
+    return dix
+###############################################################################
 
+mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 
+        'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']
+anni = [2016, 2017]
 
-def start_element(name, attrs):
-    """ Called by the parser when he meet a start element """
-    global cur_size, start
-    if start is not None:
-        # Chaining starts after each others
-        cur_file.write('<%s%s>' % (start[0], attrs_s(start[1])))
-    start = (name, attrs)
-    if ending:
-        return
-    cur_size += len(name) + sum(len(k) for k in attrs)
-    path.append((name, attrs))
-
-
-def end_element(name):
-    """ Caled by the parser when he meet an end element """
-    global cur_size
-    global start
-    if start is not None:
-        # Empty element, good, we did not wrote the start part
-        cur_file.write('<%s%s/>' % (start[0],attrs_s(start[1])))
-    else:
-        # There was some data, close it normaly
-        cur_file.write('</%s>' % name)
-    start = None
-    if ending:
-        return
-    elem = path.pop()
-    assert elem[0] == name
-    cur_size += len(name)
-    next_file()
-
-
-def char_data(data):
-    """ Called by the parser when he meet data """
-    global cur_size, start
-    wroteStart = False
-    if start is not None:
-        # The data belong to an element, we should write the start part first
-        cur_file.write('<%s%s>' % (start[0], attrs_s(start[1])))
-        start = None
-        wroteStart = True
-    # ``escape`` is too much for us, only & and < ned to be escaped there ...
-    data = data.replace('&', '&amp;')
-    data = data.replace('<', '&lt;')
-    if data == '>':
-        data = '&gt;'
-    cur_file.write(data.encode('utf-8'))
-    cur_size += len(data)
-    if not wroteStart:
-        # The data was outside of an element, it could be the right moment to
-        # make the split
-        next_file()
-
-def main(filename, output_dir):
-    # Create a parser
-    p = xml.parsers.expat.ParserCreate()
-    # We want to reproduce the input, so we are interested in the order of the
-    # attributess
-    p.ordered_attributes = 1
-
-    # Set our callbacks (we are stripping comments out by not defining
-    # callbacks for them)
-    p.XmlDeclHandler = xml_decl
-    p.StartElementHandler = start_element
-    p.EndElementHandler = end_element
-    p.CharacterDataHandler = char_data
-
-    global cur_file, cur_idx
-    global out_dir, root, ext
-
-    global FMT
-    FMT = ".%%0%dd" % (int(log10(os.path.getsize(filename) / MAX_SIZE)) + 1)
-    
-    filename2 = filename
-    out_dir, filename = os.path.split(filename)
-    if output_dir is not None:
-        out_dir = output_dir
-
-    root, ext = os.path.splitext(filename)
-
-    cur_file = open(os.path.join(out_dir, root + FMT % cur_idx + ext), 'wt')
-
-    with open(filename2, 'rt') as xml_file:
-        while True:
-            # Read a chunk
-            chunk = xml_file.read(CHUNK_SIZE)
-            if len(chunk) < CHUNK_SIZE:
-                # End of file
-                # tell the parser we're done
-                p.Parse(chunk, 1)
-                # exit the loop
-                break
-            # process the chunk
-            p.Parse(chunk)
+SOS = pd.DataFrame()
+for a in anni:
+    for mm in mesi:      
+        im = mesi.index(mm) + 1
+        sim = str(im) if len(str(im)) > 1 else "0" + str(im)
+        path = 'Z:/AREA BO&B/23.P-RNO - PD-RFO DISTRIBUTORI - BONUS/' + str(a) + '/' + sim + '. Invio di ' + mm + '/SOS/'
+        print(path)
+        if os.path.exists(path):
+            files = os.listdir(path)
+            for infile in files:
+                sos = SOSExtractor(path + infile)
+                SOS = SOS.append(sos, ignore_index = True)
             
-    # Don't forget to close our handle
-    cur_file.close()
-    
-    print("part %d Done" % cur_idx        )
-    
-    
-if __name__ == "__main__":
-    parser = OptionParser(usage="usage: %prog [options] XML_FILE")
-    parser.add_option("-o", "--output-dir",
-        help="Specify the directory where the xml files will be written" \
-            "(default to the same directory where the original file is)")
-    parser.add_option("-M", "--max_size", type="int",
-        help="Specify the size at which the files should be split (in Kb)")
-    (options, args) = parser.parse_args()
-    if len(args) != 1:
-        parser.error("incorrect number of arguments")
-    if options.max_size is not None:
-        MAX_SIZE = options.max_size * 1024
-    main(args[0], options.output_dir)
-        
-    main(infile, path2)
-        
+SOS.to_excel("SOS_elaborati.xlsx")
